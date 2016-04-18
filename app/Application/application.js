a984f56b046430345799fa83dcce5fc6d5c2ffe4
martijn0s <martijneichelsheim@hotmail.com>
2016-04-18 21:49:31 +0200

    Begin tabview

--- app/Application/application.js
+++ app/Application/application.js
@@ -1,31 +1,48 @@
-var frameModule = require("ui/frame");
+//var frameModule = require("ui/frame");
+//
+//// Accelerometer
+//exports.accelerometer = function() {
+//   frameModule.topmost().navigate("Application/accelerometer/accelerometer");
+//};
+//
+//// Camera
+//exports.camera = function() {
+//    frameModule.topmost().navigate("Application/camera/camera");
+//};
+//
+//// Bluetooth
+//exports.bluetooth = function() {
+//    frameModule.topmost().navigate("Application/bluetooth/bluetooth");
+//}
+//
+//// Trilfunctie
+//exports.vibrate = function() {
+//    frameModule.topmost().navigate("Application/vibrate/vibrate");
+//}
+//
+//// TouchId
+//exports.touchId = function() {
+//    // TODO Implement TouchId
+//}
+//
+//// GPS
+//exports.gps = function() {
+//    // TODO Implement GPS
+//}
 
-// Accelerometer
-exports.accelerometer = function() {
-   frameModule.topmost().navigate("Application/accelerometer/accelerometer");
-};
+
+var frameModule = require("ui/frame");
+var view = require("ui/core/view");
 
 // Camera
 exports.camera = function() {
     frameModule.topmost().navigate("Application/camera/camera");
 };
 
-// Bluetooth
-exports.bluetooth = function() {
-    frameModule.topmost().navigate("Application/bluetooth/bluetooth");
+function pageLoaded(args) {
+    var page = args.object;
+    var tabView1 = view.getViewById(page, "tabView1");
+    tabView1.selectedIndex = 1;
 }
+exports.pageLoaded = pageLoaded;
 
-// Trilfunctie
-exports.vibrate = function() {
-    frameModule.topmost().navigate("Application/vibrate/vibrate");
-}
-
-// TouchId
-exports.touchId = function() {
-    // TODO Implement TouchId
-}
-
-// GPS
-exports.gps = function() {
-    // TODO Implement GPS
-}
